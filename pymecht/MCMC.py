f507e3d44aaddfef5e554e04041ae0b206a681c3
Ankush Aggarwal <ankush_aggarwal2@yahoo.com>
2023-07-30 20:46:53 +0100

    Changed the internal varibles and methods to start with _ to hide them

--- pymecht/MCMC.py
+++ pymecht/MCMC.py
@@ -3,17 +3,17 @@ from tqdm import tqdm
 
 class MCMC:
     def __init__(self,prob_func,params,params_lb=None,params_ub=None,params_fix=None):
-        self.prob_func = prob_func
-        self.params,self.lb,self.ub,self.params_fix = params,params_lb,params_ub,params_fix
-        self.keys = self.params.keys()
+        self._prob_func = prob_func
+        self.params,self._lb,self._ub,self._params_fix = params,params_lb,params_ub,params_fix
+        self._keys = self.params.keys()
         self.update_ranges()
         #Test that the prob_func gives a float
-        prob_result, value = self.prob_func(self.params)
+        prob_result, value = self._prob_func(self.params)
         if not isinstance(prob_result,float):
             raise ValueError("prob_func should output a float. Instead it gave", prob_result)
         print("MCMC instance created with the following settings")
         self._param_print_transpose()
-        print(self.n_params,"parameters will be varied. For changing any of the bounds/fixed, use update_ranges function")
+        print(self._n_params,"parameters will be varied. For changing any of the bounds/fixed, use update_ranges function")
 
         self._samples = None
         self._probs = None
@@ -34,114 +34,114 @@ class MCMC:
         print("-"*len(header))
         print(header)
         print("-"*len(header))
-        for k in self.keys:
+        for k in self._keys:
             line = "{:<18}".format(k)
             line += self._format_float(self.params[k])
-            line += "{:<12}".format(str(self.params_fix[k]))
-            if self.lb is None:
-                if self.params_fix[k]:
+            line += "{:<12}".format(str(self._params_fix[k]))
+            if self._lb is None:
+                if self._params_fix[k]:
                     line += "{:<12}".format("-")
                 else:
                     line += "{:<12}".format("-inf")
             else:
-                if self.params_fix[k]:
+                if self._params_fix[k]:
                     line += "{:<12}".format("-")
                 else:
-                    line += self._format_float(self.lb[k])
-            if self.ub is None:
-                if self.params_fix[k]:
+                    line += self._format_float(self._lb[k])
+            if self._ub is None:
+                if self._params_fix[k]:
                     line += "{:<12}".format("-")
                 else:
                     line += "{:<12}".format("inf")
             else:
-                if self.params_fix[k]:
+                if self._params_fix[k]:
                     line += "{:<12}".format("-")
                 else:
-                    line += self._format_float(self.ub[k])
+                    line += self._format_float(self._ub[k])
             print(line)
         print("-"*len(header))
 
     def _param_print(self):
         header = "{:<18}".format("Keys")
-        for k in self.keys:
+        for k in self._keys:
             header += "{:<12}".format(k)
         print("-"*len(header))
         print(header)
         print("-"*len(header))
         line = "{:<18}".format("Initial value")
-        for k in self.keys:
+        for k in self._keys:
             line += self._format_float(self.params[k])
         print(line)
         line = "{:<18}".format("Fixed?")
-        for k in self.keys:
-            line += "{:<12}".format(str(self.params_fix[k]))
+        for k in self._keys:
+            line += "{:<12}".format(str(self._params_fix[k]))
         print(line)
         line = "{:<18}".format("Lower bound")
-        if self.lb is None:
-            for k in self.keys:
-                if self.params_fix[k]:
+        if self._lb is None:
+            for k in self._keys:
+                if self._params_fix[k]:
                     line += "{:<12}".format("-")
                 else:
                     line += "{:<12}".format("-inf")
         else:
-            for k in self.keys:
-                if self.params_fix[k]:
+            for k in self._keys:
+                if self._params_fix[k]:
                     line += "{:<12}".format("-")
                 else:
-                    line += self._format_float(self.lb[k])
+                    line += self._format_float(self._lb[k])
         print(line)
         line = "{:<18}".format("Upper bound")
-        if self.ub is None:
-            for k in self.keys:
-                if self.params_fix[k]:
+        if self._ub is None:
+            for k in self._keys:
+                if self._params_fix[k]:
                     line += "{:<12}".format("-")
                 else:
                     line += "{:<12}".format("inf")
         else:
-            for k in self.keys:
-                if self.params_fix[k]:
+            for k in self._keys:
+                if self._params_fix[k]:
                     line += "{:<12}".format("-")
                 else:
-                    line += self._format_float(self.ub[k])
+                    line += self._format_float(self._ub[k])
         print(line)
         print("-"*len(header))
 
     def update_ranges(self,params_lb=None,params_ub=None,params_fix=None):
         #update the bounds and fix if provided
         if params_lb is not None:
-            self.lb = params_lb
-            if self.lb.keys() != self.keys:
+            self._lb = params_lb
+            if self._lb.keys() != self._keys:
                 raise ValueError("Keys of the input lower bound of parameters do not match the model parameter keys")
 
         if params_ub is not None:
-            self.ub = params_ub
-            if self.ub.keys() != self.keys:
+            self._ub = params_ub
+            if self._ub.keys() != self._keys:
                 raise ValueError("Keys of the input upper bound of parameters do not match the model parameter keys")
 
         if params_fix is not None:
-            self.params_fix = params_fix
-            if self.params_fix.keys() != self.keys:
+            self._params_fix = params_fix
+            if self._params_fix.keys() != self._keys:
                 raise ValueError("Keys of the input params_fix do not match the model parameter keys")
 
         #set the values and bounds
-        if self.params_fix is None:
-            self.params_fix = self.params.copy()
-            for k in self.keys:
-                self.params_fix[k] = False #by default fit all parameters
-        self.n_params = np.sum(~self._dict2list(self.params_fix))
-        if self.lb is not None:
-            lb  = self._vec(self.lb)
+        if self._params_fix is None:
+            self._params_fix = self.params.copy()
+            for k in self._keys:
+                self._params_fix[k] = False #by default fit all parameters
+        self._n_params = np.sum(~self._dict2list(self._params_fix))
+        if self._lb is not None:
+            lb  = self._vec(self._lb)
         else:
-            lb  = np.array([-np.inf]*self.n_params)
-        if self.ub is not None:
-            ub      = self._vec(self.ub)
+            lb  = np.array([-np.inf]*self._n_params)
+        if self._ub is not None:
+            ub      = self._vec(self._ub)
         else:
-            ub  = np.array([np.inf]*self.n_params)
-        self.bounds = (lb,ub)
+            ub  = np.array([np.inf]*self._n_params)
+        self._bounds = (lb,ub)
         self.std = (ub-lb)/20.
 
     def _vec(self,x):
-        return np.array([value for key, value in x.items() if not self.params_fix[key]])
+        return np.array([value for key, value in x.items() if not self._params_fix[key]])
 
     def _dict2list(self,xdict):
         x = []
@@ -149,24 +149,24 @@ class MCMC:
             x.append(xdict[k])
         return np.array(x)
 
-    def proposal(self):
-        dx = np.random.normal(size=self.n_params)*self.std #could multiply by a vector of standard deviations for each parameter
+    def _proposal(self):
+        dx = np.random.normal(size=self._n_params)*self.std #could multiply by a vector of standard deviations for each parameter
         return self.c0 + dx
 
     def run(self,n):
         self.c0 = self._vec(self.params)
-        old_prob, old_value = self.prob_func(self.params)
+        old_prob, old_value = self._prob_func(self.params)
         if self._samples is None:
             self._samples = [self.c0]
             self._probs = [old_prob]
             self._values = [old_value]
         for i in tqdm(range(n)):
-            new = self.proposal()
+            new = self._proposal()
             #print(new)
-            if np.any(new < self.bounds[0]) or np.any(new > self.bounds[1]):
+            if np.any(new < self._bounds[0]) or np.any(new > self._bounds[1]):
                 continue
             self._complete_params(new) 
-            new_prob, new_value = self.prob_func(self.params)
+            new_prob, new_value = self._prob_func(self.params)
             if new_prob>=old_prob:
                 alpha = 1.
             else:
@@ -182,7 +182,7 @@ class MCMC:
     def _complete_params(self,cval):
         i=0
         for key,value in self.params.items():
-            if not self.params_fix[key]:
+            if not self._params_fix[key]:
                 try:
                     self.params[key] = cval[i]
                     i += 1
--- pymecht/ParamFitter.py
+++ pymecht/ParamFitter.py
@@ -3,18 +3,18 @@ from scipy.optimize import least_squares
 
 class ParamFitter:
     def __init__(self,sim_func,output,params,params_lb=None,params_ub=None,params_fix=None):
-        self.sim_func = sim_func
-        self.output = output
-        self.params,self.lb,self.ub,self.params_fix = params,params_lb,params_ub,params_fix
-        self.keys = self.params.keys()
+        self._sim_func = sim_func
+        self._output = output
+        self.params,self._lb,self._ub,self._params_fix = params,params_lb,params_ub,params_fix
+        self._keys = self.params.keys()
         self.update_ranges()
         #Test that the sim_func gives the same sized result as the "output"
-        sim_result = self.sim_func(self.params)
-        if sim_result.shape != self.output.shape:
-            raise ValueError("Output and simulation results are of different shape", sim_result.shape, self.output.shape)
+        sim_result = self._sim_func(self.params)
+        if sim_result.shape != self._output.shape:
+            raise ValueError("Output and simulation results are of different shape", sim_result.shape, self._output.shape)
         print("Parameter fitting instance created with the following settings")
         self._param_print_transpose()
-        print(self.n_params,"parameters will be fitted. For changing any of the bounds/fixed, use update_ranges function")
+        print(self._n_params,"parameters will be fitted. For changing any of the bounds/fixed, use update_ranges function")
 
     def _format_float(self,x):
         if (x>0.01 and x<100) or x==0:
@@ -31,113 +31,113 @@ class ParamFitter:
         print("-"*len(header))
         print(header)
         print("-"*len(header))
-        for k in self.keys:
+        for k in self._keys:
             line = "{:<18}".format(k)
             line += self._format_float(self.params[k])
-            line += "{:<12}".format(str(self.params_fix[k]))
-            if self.lb is None:
-                if self.params_fix[k]:
+            line += "{:<12}".format(str(self._params_fix[k]))
+            if self._lb is None:
+                if self._params_fix[k]:
                     line += "{:<12}".format("-")
                 else:
                     line += "{:<12}".format("-inf")
             else:
-                if self.params_fix[k]:
+                if self._params_fix[k]:
                     line += "{:<12}".format("-")
                 else:
-                    line += self._format_float(self.lb[k])
-            if self.ub is None:
-                if self.params_fix[k]:
+                    line += self._format_float(self._lb[k])
+            if self._ub is None:
+                if self._params_fix[k]:
                     line += "{:<12}".format("-")
                 else:
                     line += "{:<12}".format("inf")
             else:
-                if self.params_fix[k]:
+                if self._params_fix[k]:
                     line += "{:<12}".format("-")
                 else:
-                    line += self._format_float(self.ub[k])
+                    line += self._format_float(self._ub[k])
             print(line)
         print("-"*len(header))
 
     def _param_print(self):
         header = "{:<18}".format("Keys")
-        for k in self.keys:
+        for k in self._keys:
             header += "{:<12}".format(k)
         print("-"*len(header))
         print(header)
         print("-"*len(header))
         line = "{:<18}".format("Initial value")
-        for k in self.keys:
+        for k in self._keys:
             line += self._format_float(self.params[k])
         print(line)
         line = "{:<18}".format("Fixed?")
-        for k in self.keys:
-            line += "{:<12}".format(str(self.params_fix[k]))
+        for k in self._keys:
+            line += "{:<12}".format(str(self._params_fix[k]))
         print(line)
         line = "{:<18}".format("Lower bound")
-        if self.lb is None:
-            for k in self.keys:
-                if self.params_fix[k]:
+        if self._lb is None:
+            for k in self._keys:
+                if self._params_fix[k]:
                     line += "{:<12}".format("-")
                 else:
                     line += "{:<12}".format("-inf")
         else:
-            for k in self.keys:
-                if self.params_fix[k]:
+            for k in self._keys:
+                if self._params_fix[k]:
                     line += "{:<12}".format("-")
                 else:
-                    line += self._format_float(self.lb[k])
+                    line += self._format_float(self._lb[k])
         print(line)
         line = "{:<18}".format("Upper bound")
-        if self.ub is None:
-            for k in self.keys:
-                if self.params_fix[k]:
+        if self._ub is None:
+            for k in self._keys:
+                if self._params_fix[k]:
                     line += "{:<12}".format("-")
                 else:
                     line += "{:<12}".format("inf")
         else:
-            for k in self.keys:
-                if self.params_fix[k]:
+            for k in self._keys:
+                if self._params_fix[k]:
                     line += "{:<12}".format("-")
                 else:
-                    line += self._format_float(self.ub[k])
+                    line += self._format_float(self._ub[k])
         print(line)
         print("-"*len(header))
 
     def update_ranges(self,params_lb=None,params_ub=None,params_fix=None):
         #update the bounds and fix if provided
         if params_lb is not None:
-            self.lb = params_lb
-            if self.lb.keys() != self.keys:
+            self._lb = params_lb
+            if self._lb.keys() != self._keys:
                 raise ValueError("Keys of the input lower bound of parameters do not match the model parameter keys")
 
         if params_ub is not None:
-            self.ub = params_ub
-            if self.ub.keys() != self.keys:
+            self._ub = params_ub
+            if self._ub.keys() != self._keys:
                 raise ValueError("Keys of the input upper bound of parameters do not match the model parameter keys")
 
         if params_fix is not None:
-            self.params_fix = params_fix
-            if self.params_fix.keys() != self.keys:
+            self._params_fix = params_fix
+            if self._params_fix.keys() != self._keys:
                 raise ValueError("Keys of the input params_fix do not match the model parameter keys")
 
         #set the values and bounds
-        if self.params_fix is None:
-            self.params_fix = self.params.copy()
-            for k in self.keys:
-                self.params_fix[k] = False #by default fit all parameters
-        self.n_params = np.sum(~self._dict2list(self.params_fix))
-        if self.lb is not None:
-            lb  = self._vec(self.lb)
+        if self._params_fix is None:
+            self._params_fix = self.params.copy()
+            for k in self._keys:
+                self._params_fix[k] = False #by default fit all parameters
+        self._n_params = np.sum(~self._dict2list(self._params_fix))
+        if self._lb is not None:
+            lb  = self._vec(self._lb)
         else:
-            lb  = np.array([-np.inf]*self.n_params)
-        if self.ub is not None:
-            ub      = self._vec(self.ub)
+            lb  = np.array([-np.inf]*self._n_params)
+        if self._ub is not None:
+            ub      = self._vec(self._ub)
         else:
-            ub  = np.array([np.inf]*self.n_params)
-        self.bounds = (lb,ub)
+            ub  = np.array([np.inf]*self._n_params)
+        self._bounds = (lb,ub)
 
     def _vec(self,x):
-        return np.array([value for key, value in x.items() if not self.params_fix[key]])
+        return np.array([value for key, value in x.items() if not self._params_fix[key]])
 
     def _dict2list(self,xdict):
         x = []
@@ -146,13 +146,13 @@ class ParamFitter:
         return np.array(x)
 
     def _residual(self,cval):
-        assert(len(cval)==self.n_params)
+        assert(len(cval)==self._n_params)
         self._complete_params(cval)
-        return self.sim_func(self.params) - self.output
+        return self._sim_func(self.params) - self._output
 
     def fit(self):
         self.c0 = self._vec(self.params)
-        result = least_squares(self._residual,x0=self.c0,bounds=self.bounds,verbose=2)
+        result = least_squares(self._residual,x0=self.c0,bounds=self._bounds,verbose=2)
         print("Fitting completed, with the following results")
         self._param_print_transpose()
         return result
@@ -160,7 +160,7 @@ class ParamFitter:
     def _complete_params(self,cval):
         i=0
         for key,value in self.params.items():
-            if not self.params_fix[key]:
+            if not self._params_fix[key]:
                 try:
                     self.params[key] = cval[i]
                     i += 1
