d31c6b92ee57d8ec7d26428c7edf820c26c11acd
Ankush Aggarwal <ankush_aggarwal2@yahoo.com>
2024-07-18 15:43:01 +0100

    Fixed formatting and added the image file to pymecht's docstring

--- pymecht/__init__.py
+++ pymecht/__init__.py
@@ -1,34 +1,27 @@
 '''
 pyMechT is a Python package for simulating the mechanical response of soft biological tissues. To start using, install it via `pip` as follows
 
-.. code-black:: sh
+.. code-block:: sh
+
    $ pip install pymecht
 
-and being with the Tutorials in this documentation. 
+Tutorial provided in this documentation is a good place to start, followed by several detailed examples. 
 
 pyMechT is a collection of modules for:
 
-    * :class:`MatModels': defining material models
-    * :class:`SampleExperiment': simulating experiments, such as uniaxial extension, biaxial extension, and inflation-extension. Simulations can be either :method:`disp_controlled' or :method:`force_controlled'
-    * :class:`ParamFitter': fitting parameters to experimental data
-    * :class:`RandomParameters' and :class:`MCMC': Bayesian inference by running Monte Carlo (MC) and Markov chain Monte Carlo (MCMC) simulations
+    * :class:`MatModels`: defining material models
+    * :class:`SampleExperiment`: simulating experiments, such as uniaxial extension, biaxial extension, and inflation-extension. Simulations can be either :meth:`disp_controlled` or :meth:`force_controlled`
+    * :class:`ParamFitter`: fitting parameters to experimental data
+    * :class:`RandomParameters` and :class:`MCMC`: Bayesian inference by running Monte Carlo (MC) and Markov chain Monte Carlo (MCMC) simulations
 
-.. image:: _static/my_figure.png
+.. image:: drawing-1.svg 
   :width: 300
   :alt: Structure of pyMechT
   :align: center
 
-This package is developed and maintained by the 
+This package is developed and maintained by the `Computational Biomechanics Research Group <https://userweb.eng.gla.ac.uk/ankush.aggarwal/>`_ at the University of Glasgow and is hosted at `github <https://github.com/ankushaggarwal/pymecht/>`_.
 
-.. raw::html
-  <a href="https://userweb.eng.gla.ac.uk/ankush.aggarwal" target="_blank">Computational Biomechanics Research Grou </a>
-
-at the University of Glasgow, and is hosted at 
-
-.. raw::html
-  <a href="https://github.com/ankushaggarwal/pymecht" target="_blank">github.</a>
-
-The ethos of pyMechT is to create simplified virtual experimental setups, rather than finite element analysis. Thus, varying parameters and running simulations is much faster, allowing Bayesian inference and Markov Chain Monte Carlo.
+The ethos of pyMechT is to create simplified virtual experimental setups, rather than finite element analysis. Thus, varying parameters and running simulations is much faster, making it feasible to perform Bayesian inference and Markov Chain Monte Carlo analysis.
 
 '''
 
